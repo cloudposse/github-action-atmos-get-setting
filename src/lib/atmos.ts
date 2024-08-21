e5083e6b8fcd806703f9b7acf356ec5a5d6b401e
korenyoni <10080107+korenyoni@users.noreply.github.com>
2024-08-21 15:27:02 -0400

    Do not process templates.

--- src/lib/atmos.ts
+++ src/lib/atmos.ts
@@ -3,7 +3,9 @@ import { execSync } from "node:child_process";
 export const runAtmosDescribeComponent = async (component: string, stack: string, cwd?: string) => {
   const options = cwd ? { cwd } : {};
 
-  const command = `atmos describe component ${component} -s ${stack} --format=json`;
+  // We do not process templates in the following command, as some template commands such as 'atmos.Component'
+  // require authentication to remote state backends.
+  const command = `atmos describe component ${component} -s ${stack} --format=json --process-templates=false`;
   const atmos = execSync(command, options);
   return atmos.toString();
 };
