7e0ea7bcedef6e113ac0408b3016efc6dcf3181d
Benjamin Smith <ben.smith.developer@gmail.com>
2025-02-03 12:13:35 -0800

    bugfix: always use info level logs when parsing

--- src/lib/atmos.ts
+++ src/lib/atmos.ts
@@ -1,7 +1,7 @@
 import { execSync } from "node:child_process";
 
 export const runAtmosDescribeComponent = async (component: string, stack: string, processTemplates: boolean, cwd?: string) => {
-  const options = cwd ? { cwd } : {};
+  const options = cwd ? { cwd, env: {ATMOS_LOGS_LEVEL: "Info"} } : {env: {ATMOS_LOGS_LEVEL: "Info"}};
   const command = `atmos describe component ${component} -s ${stack} --format=json --process-templates=${String(processTemplates)}`;
   const atmos = execSync(command, options);
   return atmos.toString();
