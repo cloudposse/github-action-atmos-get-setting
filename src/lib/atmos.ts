9a74f750ad651a4dfb5576c4a93e59dabb1d642c
Igor Rodionov <goruha@gmail.com>
2025-05-10 09:39:23 +0200

    Added skip terraform functions

--- src/lib/atmos.ts
+++ src/lib/atmos.ts
@@ -1,8 +1,17 @@
 import { execSync } from "node:child_process";
 
-export const runAtmosDescribeComponent = async (component: string, stack: string, processTemplates: boolean, cwd?: string) => {
+export const runAtmosDescribeComponent = async (component: string, stack: string, processTemplates: boolean, processFunctions: boolean, cwd?: string) => {
   const options = cwd ? { cwd } : {};
-  const command = `atmos describe component ${component} -s ${stack} --format=json --process-templates=${String(processTemplates)}`;
+  let command = `atmos describe component ${component} -s ${stack} --format=json`;
+
+  if (!processFunctions) {
+    command += ` --process-functions=false`;
+  }
+
+  if (!processTemplates) {
+    command += ` --process-templates=false`;
+  }
+
   const atmos = execSync(command, options);
   return atmos.toString();
 };
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { processMultipleSettings, processSingleSetting } from "@useCase";
 (async () => {
   try {
     const processTemplates = core.getBooleanInput("process-templates");
-    const singleResult = await processSingleSetting(processTemplates);
-    const multipleResult = await processMultipleSettings(processTemplates);
+    const processFunctions = core.getBooleanInput("process-functions");
+    const singleResult = await processSingleSetting(processTemplates, processFunctions);
+    const multipleResult = await processMultipleSettings(processTemplates, processFunctions);
 
     if (singleResult || multipleResult) {
       core.info("result returned successfully");
